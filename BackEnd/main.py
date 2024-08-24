dbf8b5bcdddde86c816199a1639880fd1396d4e9
chapahrroh <chapahrroh@gmail.com>
2024-08-23 22:28:09 -0400

    añadida ruta para DB dentro de BackEnd, agregada tabla de torneos y echa la relacione entre las tablas de jugador y torneos, mas detalle en readme

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -3,49 +3,115 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from models import Jugadores, session
+from models import Players, session, Tournament
 
 app = FastAPI()
 
 
-class tabJugadores(BaseModel):
-    nombre:str
-    fechaCreacion:str|None = datetime.now().strftime("%Y-%m-%d")
-    puntuacionGeneral:int|None = 0
+class tabPlayers(BaseModel):
+    name:str
+    createDate:str|None = datetime.now().strftime("%Y-%m-%d")
+    personalScore:int|None = 0
+
+class tabTournament(BaseModel):
+    name:str
+    createDate:str|None = datetime.now().strftime("%Y-%m-%d")
 
 @app.get("/")
 def read_root():
     return {"Description": "Magic Tournament API"}
 
+# métodos relacionados con jugadores
+
 @app.get("/player")
 async def readPlayer():
-    users = session.query(Jugadores).all()
-    return [{"id":user.id, "nombre":user.nombre, "fechaCreacion":user.fechaCreacion, "puntuacionGeneral":user.puntuacionGeneral} for user in users]
+    players = session.query(Players).all()
+    return [{"id":player.id, "name":player.name, "creationDate":player.creationDate, "personalScore":player.personalScore, "Tournament": player.tournament} for player in players]
 
 @app.get("/player/{player_id}")
 async def readPlayer(player_id: int):
-    tabPlayer = session.query(Jugadores).filter(Jugadores.id == player_id).first()
-    if not tabPlayer:
-        return {"message":"No existe el usuario"}
+    player = session.query(Players).filter(Players.id == player_id).first()
+    if not player:
+        return {"message":"Player not found"}
     else:
-        return [{"id":tabPlayer.id, "nombre":tabPlayer.nombre, "fechaCreacion":tabPlayer.fechaCreacion, "puntuacionGeneral":tabPlayer.puntuacionGeneral}]
+        return [{"id":player.id, "nombre":player.name, "fechaCreacion":player.creationDate, "puntuacionGeneral":player.personalScore}]
 
 @app.post("/player")
-async def createUser(tabuser: tabJugadores):
-    newPlayer = Jugadores(
-        nombre=tabuser.nombre,
-        fechaCreacion=datetime.now(),
-        puntuacionGeneral=0
+async def createUser(tabuser: tabPlayers):
+    newPlayer = Players(
+        name=tabuser.name,
+        creationDate=datetime.now(),
+        personalScore=0
     )
     session.add(newPlayer)
     session.commit()
     return {"message": "Player added"}
 
+@app.patch("/player/{player_id}/score")
+async def updatePlayerScore(player_id: int, score: int):
+    player = session.query(Players).filter(Players.id == player_id).first()
+    if not player:
+        return {"message":"Player not found"}
+    player.personalScore = score
+    session.commit()
+    return {"message": "Player not found"}
+
 @app.delete("/player/{player_id}")
-async def deleteUser(player_id: int):
-    tabPlayer = session.query(Jugadores).filter(Jugadores.id == player_id).first()
-    if not tabPlayer:
-        return {"message":"No existe el usuario"}
-    session.delete(tabPlayer)
+async def deletePlayer(player_id: int):
+    player = session.query(Players).filter(Players.id == player_id).first()
+    if not player:
+        return {"message":"Player not found"}
+    session.delete(player)
     session.commit()
     return {"message": "Player deleted"}
+
+# métodos relacionados con Torneos
+
+@app.get("/tournament")
+async def readTournament():
+    tournaments = session.query(Tournament).all()
+    return [{"id":tournament.id, "name":tournament.name,"creation Date": tournament.creationDate, "Players": tournament.players} for tournament in tournaments]
+
+@app.get("/tournament/{tournament_id}")
+async def readTournament(tournament_id: int):
+    tournament = session.query(Tournament).filter(Tournament.id == tournament_id).first()
+    if not tournament:
+        return {"message":"Tournament not found"}
+    return [{"id":tournament.id, "name":tournament.name,"creation Date": tournament.creationDate, "Players": tournament.players}]
+
+@app.post("/tournament")
+async def createTournament(tabtournament: tabTournament):
+    newTournament = Tournament(
+        name=tabtournament.name,
+        creationDate=datetime.now(),
+        players=[]
+        )
+    session.add(newTournament)
+    session.commit()
+    return {"message": "Tournament added"}
+
+@app.post("/tournament/{tournament_id}/player/{player_id}")
+async def addPlayerToTournament(tournament_id: int, player_id: int):
+    tournament = session.query(Tournament).filter(Tournament.id == tournament_id).first()
+    if not tournament:
+        return {"message": "Tournament not found"}
+
+    player = session.query(Players).filter(Players.id == player_id).first()
+    if not player:
+        return {"message": "Player not found"}
+
+    if player in tournament.players:
+        return {"message": "This player is in the tournament"}
+
+    tournament.players.append(player)
+    session.commit()
+    return {"message": "Player added to tournament"}
+
+@app.delete("/tournament/{tournament_id}")
+async def deleteTournament(tournament_id: int):
+    tournament = session.query(Tournament).filter(Tournament.id == tournament_id).first()
+    if not tournament:
+        return {"message":"Tournament not found"}
+    session.delete(tournament)
+    session.commit()
+    return {"message": "Tournament deleted"}
--- BackEnd/models.py
+++ BackEnd/models.py
@@ -1,20 +1,47 @@
-from sqlalchemy import Column, Integer, String, DateTime, Sequence
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Sequence, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+from config import DB_PATH
 
 
-engine = create_engine('sqlite:///tournament.db')
+engine = create_engine(f'sqlite:///{DB_PATH}')
 Base = declarative_base()
 
-class Jugadores(Base):
-    __tablename__ = 'jugadores'
+# association tables
+tournamentsPlayers = Table('tournamentsPlayers', Base.metadata,
+    Column('tournament_id', Integer, ForeignKey('tournament.id')),
+    Column('player_id', Integer, ForeignKey('players.id'))
+)
+
+class Players(Base):
+    __tablename__ = 'players'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String)
-    fechaCreacion = Column(DateTime)
-    puntuacionGeneral = Column(Integer, default=0)
+    name = Column(String)
+    creationDate = Column(DateTime)
+    personalScore = Column(Integer, default=0)
+    tournament = relationship("Tournament",secondary=tournamentsPlayers, back_populates="players")
+
+    def __repr__(self):
+        return f"Players(name={self.name}, creationDate={self.creationDate}, personalScore={self.personalScore}, tournament={self.tournament})"
+
+
+class Tournament(Base):
+    __tablename__ = 'tournament'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    creationDate = Column(DateTime)
+    players = relationship("Players", secondary=tournamentsPlayers, back_populates="tournament")
+
+
+class Matches(Base):
+    __tablename__ ='matches'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
 
 Base.metadata.create_all(engine)
 
-session = sessionmaker(bind=engine)
+session = sessionmaker(bind=engine)()
